635c58a401d366616421bcabec5a6bc82755a50b
katie-sweet <145291062+katie-sweet@users.noreply.github.com>
2025-02-23 00:20:51 +0000

    Week 6 submission

--- assignments/04_revc/revc.py
+++ assignments/04_revc/revc.py
@@ -7,7 +7,6 @@ Purpose: Print the reverse complement of DNA
 
 import argparse
 import os
-#from re import A
 
 
 # --------------------------------------------------
@@ -34,51 +33,22 @@ def main():
 
     args = get_args()
     DNA = args.DNA
-    #DNA = DNA.upper()
 
     DNA_replace = {
-                        'A': 'T',
-                        'T': 'A',
-                        'C': 'G',
-                        'G': 'C',
-                        'a':'t',
-                        't': 'a',
-                        'c':'g',
-                        'g':'c'
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C",
+        "a": "t",
+        "t": "a",
+        "c": "g",
+        "g": "c",
     }
-    for char in DNA[::-1]:
-        #DNA = DNA_replace.get(char)
-        print(f"{DNA_replace.get(char)}", end="")
-        
-    #DNA_reverse = DNA[::-1]
-   # print(f"{DNA_reverse}")
 
-                    
+    for char in DNA[::-1]:
+        print(f"{DNA_replace.get(char)}", end="")
 
 
 # --------------------------------------------------
 if __name__ == "__main__":
     main()
-
-'''
-for char in DNA:
-                            if char == A:
-                                        DNA.replace("A", "t")
-                            elif char== t:
-                                        DNA.replace("t", "a")
-                            elif char == C:
-                                        DNA.replace("C", "g")
-                            elif char == G:
-                                        DNA.replace("G", "c")
-    #DNA_replace = DNA.replace("A", "t"), DNA.replace("T", "a"), DNA.replace(
-       # "C", "g"), DNA.replace("G", "c")
-
-    for char in DNA:
-        if char.upper() == A:
-            T = A
-        elif char.upper() == T:
-            A = T
-        elif char.upper() == C:
-            G = C
-        elif char.upper() == G:
-            C = G'''
